a04df7abd465c0473189001d9e8a3416d3b30f58
root <root@development.vagrant.internal>
2022-12-14 12:36:35 +0000

    latest commit

--- CentralIndex.h
+++ CentralIndex.h
@@ -1,7 +1,7 @@
-2022/12/14 12:14:44 STARTUP Redis server: tcp://127.0.0.1:6379
-2022/12/14 12:14:44 STARTUP ElasticSearch server: http://172.22.12.49:9200
-2022/12/14 12:14:44 STARTUP ES view server: http://172.22.114.129:63300/view/view
-2022/12/14 12:14:44 STARTUP CouchDB server: http://wolf_staging:******@172.22.10.201:5984
+2022/12/14 12:36:33 STARTUP Redis server: tcp://127.0.0.1:6379
+2022/12/14 12:36:33 STARTUP ElasticSearch server: http://172.22.12.49:9200
+2022/12/14 12:36:33 STARTUP ES view server: http://172.22.114.129:63300/view/view
+2022/12/14 12:36:33 STARTUP CouchDB server: http://wolf_staging:******@172.22.10.201:5984
 /**
  * Central Index
  *
